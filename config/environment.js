1126480dae439c1889c3cd6f74131aba4cd29930
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-01 12:21:01 +0100

    initial startup for backend, server is listening ok

---
+++ config/environment.js
@@ -0,0 +1,9 @@
+const dbURI = 'mongodb://localhost/sitecamp'
+const port = 4000
+const secret = 'this is a big secret'
+
+module.exports = {
+  dbURI,
+  port,
+  secret
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const logger = require('./lib/logger')
+
+const { dbURI, port } = require('./config/environment')
+
+mongoose.connect(dbURI,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('mongo connected')
+  })
+
+app.use((req, res, next) => {
+  console.log(`🚨 Incoming Request: Method: ${req.method}  URL: ${req.url}`)
+  next()
+})
+
+app.use(logger)
+
+app.listen(port, () => console.log(`Listening on localhost:${port} 🌈 `))
