e1b2e236010295d1d496d30f26e267cea571f4b3
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-02 19:35:24 +0100

    adding locations in backend data > locations

--- db/data/locations.js
+++ db/data/locations.js
@@ -4,6 +4,29 @@ module.exports = [
   {
     placeName: 'Wisconsin',
     feature: ['glamping']
+  },
+  {
+    placeName: 'Toronto',
+    feature: ['glamping']
+  },
+  {
+    placeName: 'Vegas',
+    feature: ['glamping']
+  },
+  {
+    placeName: 'Ohio',
+    feature: ['glamping']
+  },
+  {
+    placeName: 'NYC',
+    feature: ['glamping']
+  },
+  {
+    placeName: 'Manitoba',
+    feature: ['glamping']
+  },
+  {
+    placeName: 'Bear Lake',
+    feature: ['glamping']
   }
-
 ]
