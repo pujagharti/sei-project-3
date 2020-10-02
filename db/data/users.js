2a7daeade5c68841aaec603c140ce3f46cf7c9d9
puja <gc.puja1@yahoo.com>
2020-10-02 12:06:34 +0100

    updated seeds code added seeds to package .json

---
+++ db/data/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+
+  {
+    username: 'Bambino',
+    email: 'me@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    userimage: '',
+    bio: '',
+    isLocal: false,
+    usertelephone: ''
+  }
+
+]
