b184e8559361395076fb2d20a93c976cb18fef05
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-03 06:21:14 +0100

    backend changes to location model and data, adding more keys into locationsSchema

--- db/data/locations.js
+++ db/data/locations.js
@@ -1,8 +1,19 @@
 
+
 module.exports = [
 
   {
     placeName: 'Wisconsin',
+    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
+    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
+      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
+      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
+    amenities: ['shower', 'lake access', 'mornign wake-up'],
+    eventDate: '',
+    coords: {
+      latitude: 51.5074,
+      longitude: 0.1227
+    },
     feature: ['glamping']
   },
   {
--- models/location.js
+++ models/location.js
@@ -2,9 +2,19 @@
 
 const mongoose = require('mongoose')
 
+const coordsSchema = new mongoose.Schema({
+  latitude: { type: Number },
+  longitude: { type: Number } 
+})
+
 const locationsSchema = new mongoose.Schema({
   
   placeName: { type: String, unique: true },
+  placeDescription: { type: String },
+  placePhotos: [{ type: String }],
+  amenities: [{ type: String }],
+  eventDate: { type: String },
+  coords: [coordsSchema],
   feature: [{ type: String }],
   local: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
 }, {
