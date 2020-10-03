3eafb0770119675bd1d1e0ed1c45f461eab31192
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-03 15:22:18 +0100

    added location information and photo Carousel to LocationShow

---
+++ frontend/src/Components/locations/CarouselSlide.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CarouselSlide = ({ photo }) => {
+
+  
+
+
+
+  return (
+    <div>
+      <img src={photo} />
+    </div>
+  )
+
+
+}
+
+export default CarouselSlide
--- frontend/src/Components/locations/LocationShow.js
+++ frontend/src/Components/locations/LocationShow.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 import { getSingleLocation } from '../../lib/api'
+import CarouselSlide from './CarouselSlide'
 
 class LocationShow extends React.Component {
 
@@ -23,8 +26,43 @@ class LocationShow extends React.Component {
 
     if (!this.state.locationData) return <h1>Trying to find that for you now</h1>
 
+    const { placePhotos, placeName, placeDescription, amenities } = this.state.locationData
+    const features = this.state.locationData.feature
     return (
-      <h1>Location SHOW!</h1>
+      <>
+        <div className='location-show-container'>
+          <div className='carousel-container'>
+            <Carousel showThumbs={false}>
+              {placePhotos.map((photo, index) => {
+                return <CarouselSlide key={index} photo={photo} />
+              })}
+            </Carousel>
+          </div>
+
+          <div className='location-show-info'>
+            
+            <h1>{placeName}</h1>
+            <h2>{placeDescription}</h2>
+            
+            <p>Has:
+              {
+                features.map((element, index) => {
+                  return <div key={index}>{element}</div>
+                })
+              }
+            </p>
+
+            <p>Amenities:
+              {
+                amenities.map((amenity, index) => {
+                  return <div key={index}>{amenity}</div>
+                })
+              }
+            </p>
+          </div>
+        </div>
+
+      </>
     )
   }
 
