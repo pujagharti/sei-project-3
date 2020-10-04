718c790540a2574c112d0a41cb61f06041db38a2
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-04 08:31:13 +0100

    adding Comment Group and LocationSingleComment, currently using data inserted into state as an example

--- frontend/src/Components/locations/LocationComments.js
+++ frontend/src/Components/locations/LocationComments.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import { Button, Comment, Form, Header } from 'semantic-ui-react'
 
+import LocationSingleComment from './LocationSingleComment'
+
 
 class LocationComments extends React.Component {
 
+  state = {
+    comments: [
+      {
+        text: 'Great place to stay! Loved it!',
+        rating: 4,
+        local: {
+          _id: '5f78b33f26d5042567fc6098',
+          username: 'Jess_Reinger',
+          email: 'Presley.Hickle@email.com',
+          userimage: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+          bio: '',
+          isLocal: false
+        }
+      },
+      {
+        text: 'OK, but too expensive for what it is!',
+        rating: 3,
+        local: {
+          _id: '5f78b33f26d5042567fc6099',
+          username: 'Casimir.Doyle',
+          email: 'Ebony.Botsford@email.com',
+          userimage: 'https://s3.amazonaws.com/uifaces/faces/twitter/frankiefreesbie/128.jpg',
+          bio: '',
+          isLocal: false
+        }
+      }
+    ]
+  }
 
   render() {
 
-    const {comments} = this.props.comments
+    // const {comments} = this.props.comments
+    const { comments } = this.state
 
     return (
 
@@ -16,7 +47,10 @@ class LocationComments extends React.Component {
           What people say
         </Header>
 
-        
+        {comments.map((comment, index)=>{
+          return <LocationSingleComment key={index} { ...comment }/>
+        })
+        }
 
         <Form reply>
           <Form.TextArea />
---
+++ frontend/src/Components/locations/LocationSingleComment.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Comment } from 'semantic-ui-react'
+
+const LocationSingleComment = (props) => {
+
+
+  const { text, rating } = props
+
+  return (
+
+    <Comment>
+      <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+      <Comment.Content>
+        <Comment.Author as='a'>Matt</Comment.Author>
+        <Comment.Metadata>
+          <div>Today at 5:42PM</div>
+        </Comment.Metadata>
+        <Comment.Text>{text}</Comment.Text>
+        <Comment.Actions>
+          <Comment.Action>Reply</Comment.Action>
+        </Comment.Actions>
+      </Comment.Content>
+    </Comment>
+  )
+}
+
+export default LocationSingleComment
