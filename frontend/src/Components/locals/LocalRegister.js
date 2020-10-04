6c8694900189973d445bc905a4bb5d52102cef7a
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-04 19:59:05 +0100

    adding auth check in LocalRegister

--- frontend/src/Components/locals/LocalRegister.js
+++ frontend/src/Components/locals/LocalRegister.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import { Form, Input, TextArea, Button } from 'semantic-ui-react'
 
 import { registerUser } from '../../lib/api'
+import { isAuthenticated } from '../../lib/auth'
 
 class LocalRegister extends React.Component {
-  state = { 
+  state = {
     formData: {
       email: '',
       username: '',
       password: '',
       passwordConfirmation: '',
       bio: ''
-    }
+    },
+    redirect: null
   }
 
+  authenticated() {
+    return isAuthenticated()
+  }
 
   handleChange = (e) => {
     const formData = {
@@ -27,61 +33,88 @@ class LocalRegister extends React.Component {
 
   handleSubmit = async (e) => {
     e.preventDefault()
-    const dataToSend = ({ ...this.state.formData, isLocal: true })    
-    try {
-      const res = await registerUser(dataToSend)
-      console.log(res)
-    } catch (err) {
-      console.log(err)
+    const authenticated = this.authenticated()
+
+    if (!authenticated) {
+      const dataToSend = ({ ...this.state.formData, isLocal: true })
+      try {
+        await registerUser(dataToSend)
+
+        this.setState({
+          redirect: '/login'
+        })
+        return
+      } catch (err) {
+        console.log(err)
+      }
     }
+
+    if (authenticated) {
+      try {
+        console.log('TESTING!')
+      } catch (err){
+        console.log(err)
+      }
+    }
+
+
+
   }
 
   render() {
     const { username, email, password, passwordConfirmation, bio } = this.state.formData
 
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
+    }
+
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Form.Group widths='equal'>
-          <Form.Field
-            control={Input}
-            label='User name'
-            placeholder='User name'
-            onChange={this.handleChange}
-            name='username'
-            value={username}
-          />
-          <Form.Field
-            control={Input}
-            label='Email'
-            placeholder='Email'
-            onChange={this.handleChange}
-            name='email'
-            value={email}
+        { !this.authenticated() &&
+          <Form.Group widths='equal'>
+            <Form.Field
+              control={Input}
+              label='User name'
+              placeholder='User name'
+              onChange={this.handleChange}
+              name='username'
+              value={username}
+            />
+            <Form.Field
+              control={Input}
+              label='Email'
+              placeholder='Email'
+              onChange={this.handleChange}
+              name='email'
+              value={email}
+            />
+            <Form.Field
+              control={Input}
+              label='Password'
+              placeholder='Password'
+              onChange={this.handleChange}
+              name='password'
+              value={password}
+            />
+            <Form.Field
+              control={Input}
+              label='Password confirmation'
+              placeholder='Password confirmation'
+              onChange={this.handleChange}
+              name='passwordConfirmation'
+              value={passwordConfirmation}
+            />
+          </Form.Group>
+        }
 
-          />
-          <Form.Field
-            control={Input}
-            label='Password'
-            placeholder='Password'
-            onChange={this.handleChange}
-            name='password'
-            value={password}
-        
-
-          />
-          <Form.Field
-            control={Input}
-            label='Password confirmation'
-            placeholder='Password confirmation'
-            onChange={this.handleChange}
-            name='passwordConfirmation'
-            value={passwordConfirmation}
-
-          />
-        </Form.Group>
         <Form.Group inline>
 
         </Form.Group>
+
+        {this.authenticated() &&
+          <h3>Thanks for your interest in contributing! Just a bit more about you, and we can get your profile set up </h3>
+        }
+
         <Form.Field
           control={TextArea}
           label='About'
--- frontend/src/lib/auth.js
+++ frontend/src/lib/auth.js
@@ -3,3 +3,28 @@
 export function setToken(token){
   localStorage.setItem('token', token)
 }
+
+export const getToken = () =>{
+  return localStorage.getItem('token')
+}
+
+export function logout(){
+  localStorage.removeItem('token')
+}
+
+const getPayload = () => {
+  const token = getToken()
+  if (!token) return false
+  const parts = token.split('.')
+  if (parts.length < 3) return false
+  
+  return JSON.parse(atob(parts[1]))
+}
+
+export const isAuthenticated = () => {
+  const payload = getPayload()
+  if (!payload) return false
+  const now = Math.floor(Date.now() / 1000)
+  console.log(now < payload.exp)
+  return now < payload.exp
+}
