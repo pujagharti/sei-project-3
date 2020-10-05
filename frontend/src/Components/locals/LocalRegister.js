aa9fddc42c3f3caba65e8bc66fd7aab346ff9fde
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-05 15:07:01 +0100

    resolved merge conflicts

--- frontend/src/Components/locals/LocalRegister.js
+++ frontend/src/Components/locals/LocalRegister.js
@@ -1,10 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Redirect } from 'react-router-dom'
-import { Form, Input, TextArea, Button, Grid, Header } from 'semantic-ui-react'
-=======
 import { Form, Input, TextArea, Button, Grid, Header, Image } from 'semantic-ui-react'
->>>>>>> homepage-styling
 
 import { registerUser, updateUser, getUserProfile } from '../../lib/api'
 import { isAuthenticated } from '../../lib/auth'
@@ -98,49 +94,6 @@ class LocalRegister extends React.Component {
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as='h2' color='black' textAlign='center'>
-<<<<<<< HEAD
-            {/* <Image src='/logo.png' />  */}
-          Log-in to your account
-        </Header>
-          <Form onSubmit={this.handleSubmit}>
-            {!this.authenticated() &&
-              <Form.Group widths='equal'>
-                <Form.Field
-                  control={Input}
-                  label='User name'
-                  placeholder='User name'
-                  onChange={this.handleChange}
-                  name='username'
-                  value={username}
-                />
-                <Form.Field
-                  control={Input}
-                  label='Email'
-                  placeholder='Email'
-                  onChange={this.handleChange}
-                  name='email'
-                  value={email}
-                />
-                <Form.Field
-                  control={Input}
-                  label='Password'
-                  placeholder='Password'
-                  onChange={this.handleChange}
-                  name='password'
-                  value={password}
-                />
-                <Form.Field
-                  control={Input}
-                  label='Password confirmation'
-                  placeholder='Password confirmation'
-                  onChange={this.handleChange}
-                  name='passwordConfirmation'
-                  value={passwordConfirmation}
-                />
-              </Form.Group>
-            }
-
-=======
             <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_0XVXWXbh6quw4pprg2muCVE-P3Jt_aG8JQ&usqp=CAU' /> 
         Register as a Local
           </Header>
@@ -183,7 +136,6 @@ class LocalRegister extends React.Component {
 
               />
             </Form.Group>
->>>>>>> homepage-styling
             <Form.Group inline>
 
             </Form.Group>
