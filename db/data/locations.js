f4cbdfd48880ff6daecc377c57593492aaf3695f
puja <gc.puja1@yahoo.com>
2020-10-05 20:18:16 +0100

    added features css

--- db/data/locations.js
+++ db/data/locations.js
@@ -2,84 +2,162 @@
 
 module.exports = [
 
+  //NightLife//
   {
     placeName: 'Wisconsin',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: {
-      latitude: 55.1725,
-      longitude: -118.8012
-    },
-    feature: ['glamping']
+    placePhotos: ['https://bloximages.chicago2.vip.townnews.com/madison.com/content/tncms/assets/v3/editorial/e/1b/e1bbad78-5edd-5522-9720-c4d61cd7721d/5d59f89720fae.image.jpg?resize=1200%2C827'],
+    feature: ['NightLife']
   },
   {
-    placeName: 'Deapcroft Woods Camping',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: { latitude: 57.849494951149765, longitude: -118.07916718606882 },
-    feature: ['glamping']
-  },
-  {
-    placeName: 'Foggy Farm',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: { latitude: 52.65976935587413, longitude: -105.57792878190138 },
-    feature: ['glamping']
-  },
-  {
-    placeName: 'Twenty-mile Meadow',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: { latitude: 55.478482913696965, longitude: -112.57284979993919 },
-    feature: ['glamping']
-  },
-  {
-    placeName: 'Bearwood',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: { latitude: 59.282169050110696, longitude: -107.2451450098714 },
-    feature: ['glamping']
-  },
-  {
-    placeName: 'Magical place in the woods',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: { latitude: 54.09723979305515, longitude: -107.21921302584624 },
-    feature: ['glamping']
-  },
-  {
-    placeName: 'Bleakburn place',
-    placeDescription: 'A modern glamping experience in the heart of amazing woodland',
-    placePhotos: ['https://imageresizer.static9.net.au/AecMpvdvP4gXYrkC3B-LZUlP2nA=/400x0/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2018%2F02%2F06%2F14%2F34%2FINTRO.jpg',
-      'https://img.huffingtonpost.com/asset/5cd5264a2000005c00969e0a.jpeg?ops=scalefit_720_noupscale',
-      'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/05/31746323273_f84b91b9b2_b.jpg'],
-    amenities: ['shower', 'lake access', 'mornign wake-up'],
-    eventDate: '',
-    coords: { latitude: 52.78537333971177, longitude: -101.3340705038111 },
-    feature: ['glamping']
-  }
-]
+  placeName: '',
+  placePhotos: ['https://assets.visitmilwaukee.org/visitmke/media/global/Partner%20Images/Gallery%20Size/VM-HiHatGarage-Interior-Guests.jpg?ext=.jpg&mode=crop&width=960&height=540&scale=both&quality=80'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2rrvBhgNTDg6rWutDronhmtLYtIz-4MadIw&usqp=CAU'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQRlPaGfL-4NBLfOnBcRJJanRBWgtY3OOCfqw&usqp=CAU'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://i.pinimg.com/originals/9c/d4/d9/9cd4d97b69db4c8eb1da386416170e9b.jpg'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSkjrtAfeVuttRrl8iKpdDYeKbCS2RajV6bKw&usqp=CAU'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJReefAHX7UnpyJkU6LTxO8-x7k2oVAECopw&usqp=CAU'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://www.dells.com/wp-content/uploads/2015/03/marleys-crowd.jpg'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://www.gannett-cdn.com/-mm-/e4942202d84eff1a81c55af30fd86506fbc0c85c/c=0-300-3600-2334/local/-/media/2018/05/24/WIGroup/Appleton/636627760405103182-APCBrd-08-03-2014-Crescent-1-A013-2014-08-01-IMG-APC-four-weeks-out-f-1-1-J081N1PA-L455794076-IMG-APC-four-weeks-out-f-1-1-J081N1PA.jpg'],
+  feature: ['NightLife']
+},
+{
+  placeName: '',
+  placePhotos: ['https://www.gannett-cdn.com/-mm-/9a10d18443197ab9a2a837953a60ea6375b5467c/c=4-0-1916-1080/local/-/media/2015/06/29/WIGroup/WisconsinRapids/635711796582471243-Fireworks-Pic.JPG'],
+  feature: ['NightLife']
+},
+
+    // Summer //
+
+
+{
+    placeName: '',
+    placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzlk3askxG-FW12fsIruK4-EfgZa5gPP4euA&usqp=CAU'],
+    feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRTEyBZL3GQckErG6yFhwLmhHa0likp-cmAQA&usqp=CAU'],
+  feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQbxNT8w_N1WG8DOaCQdMSWsgHoLk6omZE0WA&usqp=CAU'],
+  feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://pbs.twimg.com/media/D59JeY4WsAABVgk.jpg'],
+  feature: ['Summer']
+},
+{
+placeName: '',
+placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfX4ZG3JqgFiHIpQmru8TR2rBmfFRK-pGw9A&usqp=CAU'],
+feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3-PGvZJzMsu1oWPGBBPa2GENxGy5Dzc4Mdg&usqp=CAU'],
+  feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVkrEEK9DZZ9yFfV7LO_DaiFIaeHzAT7jOqw&usqp=CAU'],
+  feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://www2.safetyserve.com/wp-content/uploads/2018/02/safety-serve-article-summer-alcohol-safety.jpg'],
+  feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://f2z5y7q2.rocketcdn.me/lib/wp-content/uploads/2016/06/bigstock-Friends-having-fun-together-at-120587672-e1466618093682.jpg'],
+  feature: ['Summer']
+},
+{
+  placeName: '',
+  placePhotos: ['https://www.luxurylifestylemag.co.uk/wp-content/uploads/2020/07/bigstock-Family-Celebration-Outside-In-229972240.jpg'],
+  feature: ['Summer']
+},
+
+
+// GoWild//
+
+{
+  placeName: '',
+  placephotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTbddEm8JTjH5_IxqcjhgDKEl3IHIlWG-7UcA&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS45BdhD6_BUDbGgGyFJYQ1lNabQVMEDHBf4g&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuvMb7nKIppDlZA9UUuJAu0W82KpDO8nY2eA&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSrws4B9p6qWR6hh5FqjVWMG3pmZ0xREXcs9Q&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSNnb2tRCY70k907-1Zb0JDeg-iimgCKvo3sg&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ9q5Jlcj33yTvDrDe4oLUIPKydnX3f1KoOlQ&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://boundlessoutdoors.co.uk/wp-content/uploads/2020/06/Archery-1.jpg'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiQSzu6kRH6-NAqwaeJabloGcabVzcnhvrhA&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTd_v_0-HsTAqQyR2ryTvbEnUsY8PH0-mDGJw&usqp=CAU'],
+  feature: ['GoWild']
+},
+{
+  placeName: '',
+  placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDtjhporhx4jj7cCqlDa4m29gXxCFkU6uMaA&usqp=CAU'],
+  feature: ['GoWild']
+}
