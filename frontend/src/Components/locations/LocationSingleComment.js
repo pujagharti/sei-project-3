2a480a92cf3184633a7fffb6c165a1e7d8a696af
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-05 16:34:00 +0100

    fixed timestamp on comments

--- frontend/src/Components/locations/LocationSingleComment.js
+++ frontend/src/Components/locations/LocationSingleComment.js
@@ -6,13 +6,15 @@ const LocationSingleComment = (props) => {
 
   const { text, rating, createdAt } = props
 
+  const createdAtSliced = createdAt.slice(0, 10)
+
   return (
     <Comment>
       <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
       <Comment.Content>
         <Comment.Author as='a'><span>{props.local.username}</span></Comment.Author>
         <Comment.Metadata>
-          <div>{createdAt}</div>
+          <div>{createdAtSliced}</div>
         </Comment.Metadata>
         <Comment.Text>{text}</Comment.Text>
         <Comment.Text>Rating: {rating}</Comment.Text>
