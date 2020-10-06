9d3644cb1e393ebb033ee454b4a9ecee1940af32
Anouska Ralph <anouskaoleary@gmail.com>
2020-10-06 13:59:01 +0100

    seeded nightlife

--- db/data/locations.js
+++ db/data/locations.js
@@ -121,58 +121,119 @@ module.exports = [
       longitude: -72.987789
     },
     feature: ['gowild']
+  },
+  //NightLife//
+  {
+    placeName: 'Festival of Lights',
+    placeDescription: 'The festival is spread across the city, with many locations',
+    placePhotos: ['https://www.loewshotels.com/blog/wp-content/uploads/2016/01/MONTREAL_EN_LUMIERE-1-625X417.jpg'],
+    amenities: ['Food', 'Rides', 'Drink', 'Disabled Access'],
+    coords: {
+      latitude: 45.498623,
+      longitude: -73.576963
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'The Comedy Nest ',
+    placeDescription: 'Come and enjoy a show in a cabaret setting, with candle-lit tables, full-service bar, and wonderful wait-staff.',
+    placePhotos: ['https://blog.viarail.ca/wp-content/uploads/2019/03/judah.jpg'],
+    amenities: ['Food', 'Drink', 'Disabled Access'],
+    coords: {
+      latitude: 45.489816,
+      longitude: -73.583984
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'Cabaret Show',
+    placeDescription: 'Festive gender-bending performances are a draw at this gay-friendly, 1920s-inspired nightspot.',
+    placePhotos: ['https://www.bonjourquebec.com/en/listing/images/master/9188d5cb-f6cc-493b-9682-f41bdac9ce9f/cabaret-lion-dor-cabaret-lion-dor.jpg'],
+    amenities: ['Food', 'Drink', 'Disabled Access'],
+    coords: {
+      latitude: 45.510163,
+      longitude: -73.564333
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'Théâtre Maisonneuve',
+    placeDescription: 'A modern version of the Italian-style theatre, Théâtre Maisonneuve is equipped with a large and highly versatile multifunctional stage',
+    placePhotos: ['https://moriartyland.net/site/wp-content/uploads/2014/07/20140717-112420-41060762.jpg'],
+    amenities: ['Food', 'Drink', 'Disabled Access'],
+    coords: {
+      latitude: 45.509270,
+      longitude: -73.566068
+    },
+    feature: ['NightLife']
+  }, 
+  {
+    placeName: 'Montreal Food Tours',
+    placeDescription: 'Food tours take place across Mile-End, a foody hotspot.',
+    placePhotos: ['https://localfoodtours.com/wp-content/uploads/2017/05/Montrael-things-to-do-Evening-Mile-End-Montreal-Food-Tour.jpg'],
+    amenities: ['Food', 'Drink'],
+    coords: {
+      latitude: 45.519810,
+      longitude: -73.595941
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'La Voûte',
+    placeDescription: 'La Voûte stands out thanks to its artistic direction and different shows offered every night including artistic performances, unusual shows that offer a mystifying and avant-garde experience in one of the most emblematic places in the island of Montreal.',
+    placePhotos: ['https://media-cdn.tripadvisor.com/media/photo-s/18/97/f9/b1/marcus-montreal.jpg'],
+    amenities: ['Food', 'Drink'],
+    coords: {
+      latitude: 45.519810,
+      longitude: -73.595941
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'Ausgang',
+    placeDescription: 'Ausgang is a non-profit organization working in the dissemination and production of initiatives encouraging local culture, while offering a cosmopolitan offer. We promote collaborations, in order to forge links between the various Montreal players',
+    placePhotos: ['https://images.squarespace-cdn.com/content/v1/52fab727e4b06b4beaf4662d/1490728152980-AXB4D0K9XU88XZW5HI7K/ke17ZwdGBToddI8pDm48kCX-V5vw-8h9IBXN10-_8XN7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0p4Wyba38KfG317vYluk45_zZdtnDCZTLKcP2mivxmYi50xvY5saIGKMgOza9mH4XA/image-asset.jpeg'],
+    amenities: ['Food', 'Drink', 'Community Space'],
+    coords: {
+      latitude: 45.536001,
+      longitude: -73.603306
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'Turbo Haüs',
+    placeDescription: 'The preeminent place to be for heavy rock, punk and metal shows on the smaller side.',
+    placePhotos: ['https://static1.squarespace.com/static/5514c867e4b088ff856d4b55/t/5dfa76ff7748b21c82555743/1576695555204/131-turbo+haus-low+res-photo+susan+moss.jpg?format=1500w'],
+    amenities: ['Food', 'Drink', 'Music venue'],
+    coords: {
+      latitude: 45.536001,
+      longitude: -73.603306
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'Stereo After Hours ',
+    placeDescription: 'Stereo nightclub is an afterhours club in Montreal, Quebec, Canada which primarily features house music and techno. The afterhours section of the club is always open on Fridays and Saturdays.',
+    placePhotos: ['https://whenwedip.com/wp-content/uploads/2019/12/Stereo.jpg'],
+    amenities: ['Food', 'Drink', 'Music venue', 'Night club'],
+    coords: {
+      latitude: 45.515791,
+      longitude: -73.558086
+    },
+    feature: ['NightLife']
+  },
+  {
+    placeName: 'Joverse',
+    placeDescription: 'Hip, industrial-chic spot serving creative cocktails & refined Californian fare with a Latin twist.',
+    placePhotos: ['https://s3.amazonaws.com/somontrealapp/images/20160211-56bcaa465baf9.jpg'],
+    amenities: ['Food', 'Drink', 'Cocktail bar'],
+    coords: {
+      latitude: 45.515791,
+      longitude: -73.558086
+    },
+    feature: ['NightLife']
   }
-  // //NightLife//
-  // {
-  //   placeName: 'Wisconsin',
-  //   placePhotos: ['https://bloximages.chicago2.vip.townnews.com/madison.com/content/tncms/assets/v3/editorial/e/1b/e1bbad78-5edd-5522-9720-c4d61cd7721d/5d59f89720fae.image.jpg?resize=1200%2C827'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://assets.visitmilwaukee.org/visitmke/media/global/Partner%20Images/Gallery%20Size/VM-HiHatGarage-Interior-Guests.jpg?ext=.jpg&mode=crop&width=960&height=540&scale=both&quality=80'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2rrvBhgNTDg6rWutDronhmtLYtIz-4MadIw&usqp=CAU'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQRlPaGfL-4NBLfOnBcRJJanRBWgtY3OOCfqw&usqp=CAU'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://i.pinimg.com/originals/9c/d4/d9/9cd4d97b69db4c8eb1da386416170e9b.jpg'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSkjrtAfeVuttRrl8iKpdDYeKbCS2RajV6bKw&usqp=CAU'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJReefAHX7UnpyJkU6LTxO8-x7k2oVAECopw&usqp=CAU'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://www.dells.com/wp-content/uploads/2015/03/marleys-crowd.jpg'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://www.gannett-cdn.com/-mm-/e4942202d84eff1a81c55af30fd86506fbc0c85c/c=0-300-3600-2334/local/-/media/2018/05/24/WIGroup/Appleton/636627760405103182-APCBrd-08-03-2014-Crescent-1-A013-2014-08-01-IMG-APC-four-weeks-out-f-1-1-J081N1PA-L455794076-IMG-APC-four-weeks-out-f-1-1-J081N1PA.jpg'],
-  //   feature: ['NightLife']
-  // },
-  // {
-  //   placeName: '',
-  //   placePhotos: ['https://www.gannett-cdn.com/-mm-/9a10d18443197ab9a2a837953a60ea6375b5467c/c=4-0-1916-1080/local/-/media/2015/06/29/WIGroup/WisconsinRapids/635711796582471243-Fireworks-Pic.JPG'],
-  //   feature: ['NightLife']
-  // },
+
 
   // // Summer //
 
@@ -226,7 +287,7 @@ module.exports = [
   //   placePhotos: ['https://www.luxurylifestylemag.co.uk/wp-content/uploads/2020/07/bigstock-Family-Celebration-Outside-In-229972240.jpg'],
   //   feature: ['Summer']
   // },
-  
+
   // // GoWild//
 
   // {
