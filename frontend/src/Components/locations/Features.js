6a0f5bdd614401014c270f3c85cc1f62b5c167ff
puja <gc.puja1@yahoo.com>
2020-10-06 01:05:13 +0100

    changes to indexpage

--- frontend/src/Components/locations/Features.js
+++ frontend/src/Components/locations/Features.js
@@ -7,9 +7,9 @@ const Features = () => {
   return (
 
     <section className='features-container'>
-      <Link to="" className='features-nightLife'>Night Life</Link>
+      <Link to="/" className='features-nightLife'>Night Life</Link>
       <Link to="/features/summer" className='features-summer'>Summer</Link>
-      <Link to="" className='features-goWild'>Go Wild</Link>
+      <Link to="/features/gowild" className='features-goWild'>Go Wild</Link>
     </section>
 
 
