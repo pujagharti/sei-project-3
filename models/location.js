5920a2c78d3db8eacd03adfac0e7a2eee6167732
teresa <teresajclark@gmail.com>
2020-10-06 22:50:52 +0100

    Additional comments added, added to seedexternal script

--- models/location.js
+++ models/location.js
@@ -8,7 +8,7 @@ const coordsSchema = new mongoose.Schema({
 })
 
 const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true, maxlength: 300 },
+  text: { type: String, required: true, maxlength: 400 },
   rating: { type: Number, required: true, min: 1, max: 5 },
   local: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
 }, {
