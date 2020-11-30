f864841166f485f7fd570b990b29d273e68d8a07
puja <gc.puja1@yahoo.com>
2020-11-30 22:51:03 +0000

    public deployement

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const dbURI = 'mongodb://localhost/sitecamp'
+const dbURI = 'mongodb+srv://puja:Poojja@cluster0.9lbdg.mongodb.net/montreal?retryWrites=true&w=majority'
 const port = process.env.PORT || 4000
 const secret = 'this is a glamper secret'
 
